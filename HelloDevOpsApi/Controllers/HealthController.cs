0a9a4b703e2db1fe58c5b5d5b77c7c88ca99602f
Nawaaf123 <nawaafmohd22@gmail.com>
2025-12-15 22:12:32 -0600

    Add project files.

---
+++ HelloDevOpsApi/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloDevOpsApi.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "Healthy",
+                service = "HelloDevOpsApi",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}
